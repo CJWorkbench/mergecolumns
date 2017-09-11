48f4cce8aa9a98094215654b30bddaf0ee8f47cb
alexgonca <alexandre.reportagem@gmail.com>
2017-09-11 10:22:30 -0400

    Initialize the repository

---
+++ mergecolumns.py
@@ -0,0 +1,23 @@
+class Importable:
+    @staticmethod
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def event():
+        pass
+
+    @staticmethod
+    def render(wf_module, table):
+        firstcol = wf_module.get_param_column('firstcolumn')
+        secondcol = wf_module.get_param_column('secondcolumn')
+        delimiter = wf_module.get_param_string('delimiter')
+        newcol = wf_module.get_param_string('newcolumn')
+
+        if firstcol == '' or secondcol == '':
+            return table
+
+        wf_module.set_ready(notify=False)
+
+        table[newcol] = table[firstcol, secondcol].apply(lambda x: delimiter.join([str(i) for i in x if str(i) != 'nan']), axis=1)
+        return table
