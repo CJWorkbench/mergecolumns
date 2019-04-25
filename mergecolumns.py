5b206bf96161cd91e61f4031e2367b7218aa9dcb
Adam Hooper <adam@adamhooper.com>
2019-04-25 17:01:43 -0400

    Allow merging the value, "nan"
    The previous logic was not justified.

--- mergecolumns.py
+++ mergecolumns.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def render(table, params):
     if table is None:
         return None
@@ -7,8 +10,24 @@ def render(table, params):
     delimiter = params['delimiter']
     newcol = params['newcolumn']
 
-    if firstcol == '' or secondcol == '' or newcol == '':
+    if not firstcol or not secondcol or not newcol:
         return table
 
-    table[newcol] = table[[firstcol, secondcol]].apply(lambda x: delimiter.join([str(i) for i in x if str(i) != 'nan']), axis=1)
+    col1 = table[firstcol]
+    col2 = table[secondcol]
+    na1 = col1.isnull()
+    na2 = col2.isnull()
+
+    if hasattr(col1, 'cat'):
+        col1 = col1.astype(str)
+        col1[na1] = np.nan
+    if hasattr(col2, 'cat'):
+        col2 = col2.astype(str)
+        col2[na2] = np.nan
+
+    result = col1 + delimiter  # np.nan in col1 will stay na
+    result[na1 & ~na2] = '' # so we can add col2 when it is not nan
+    result[~na2] += col2
+
+    table[newcol] = result
     return table
--- test_mergecolumns.py
+++ test_mergecolumns.py
@@ -1,74 +1,131 @@
 import unittest
-import pandas as pd
 import numpy as np
+import pandas as pd
+from pandas.testing import assert_frame_equal
 from mergecolumns import render
 
 
+def P(firstcolumn=None, secondcolumn=None, delimiter='-', newcolumn='New column'):
+    return {
+        'firstcolumn': firstcolumn,
+        'secondcolumn': secondcolumn,
+        'delimiter': delimiter,
+        'newcolumn': newcolumn,
+    }
+
+
 class TestMergeColumns(unittest.TestCase):
+    def test_defaults_no_op(self):
+        result = render(
+            pd.DataFrame({'A': ['a', 'b'], 'B': ['b', 'c']}),
+            P()
+        )
+        expected = pd.DataFrame({'A': ['a', 'b'], 'B': ['b', 'c']})
+        assert_frame_equal(result, expected)
 
-    def setUp(self):
-        # Test data includes:
-        #  - rows of numeric, string, and categorical types
-        #  - zero entries (which should not be removed)
-        #  - if either column categorical type, retain caegorical
-        self.table = pd.DataFrame([
-            ['a',    1,  2,   'b',    '', 1],
-            ['a',   1,  None,   'b',  'b',    None],
-            ['a',    1,    None,   '', 'c',    2],
-            ['a',    1, 2, '', 'd', None],
-            ['a',  1,  2,  'b',    'e',    3]],
-            columns=['stringcol1','intcol','floatcol','stringcol2','catcol','floatcatcol'])
+    def test_no_firstcolumn_no_op(self):
+        result = render(
+            pd.DataFrame({'A': ['a', 'b'], 'B': ['b', 'c']}),
+            P(secondcolumn='B', newcolumn='C')
+        )
+        expected = pd.DataFrame({'A': ['a', 'b'], 'B': ['b', 'c']})
+        assert_frame_equal(result, expected)
 
-        # Pandas should infer these types anyway, but leave nothing to chance
-        self.table['stringcol1'] = self.table['stringcol1'].astype(str)
-        self.table['intcol'] = self.table['intcol'].astype(np.int64)
-        self.table['floatcol'] = self.table['floatcol'].astype(np.float64)
-        self.table['stringcol2'] = self.table['stringcol2'].astype(str)
-        self.table['catcol'] = self.table['catcol'].astype('category')
-        self.table['floatcatcol'] = self.table['floatcatcol'].astype('category')
+    def test_no_secondcolumn_no_op(self):
+        result = render(
+            pd.DataFrame({'A': ['a', 'b'], 'B': ['b', 'c']}),
+            P(firstcolumn='A', newcolumn='C')
+        )
+        expected = pd.DataFrame({'A': ['a', 'b'], 'B': ['b', 'c']})
+        assert_frame_equal(result, expected)
 
-    def test_NOP(self):
-        params = { 'firstcolumn': '', 'secondcolumn': 'intcol', 'newcolumn': 'intcol2', 'delimiter': '-'}
-        out = render(self.table, params)
-        self.assertTrue(out.equals(self.table)) # should NOP when first applied
+    def test_no_newcolumn_no_op(self):
+        result = render(
+            pd.DataFrame({'A': ['a', 'b'], 'B': ['b', 'c']}),
+            P(firstcolumn='A', secondcolumn='B', newcolumn=None)
+        )
+        expected = pd.DataFrame({'A': ['a', 'b'], 'B': ['b', 'c']})
+        assert_frame_equal(result, expected)
 
-        params = { 'firstcolumn': 'intcol', 'secondcolumn': '', 'newcolumn': 'intcol2', 'delimiter': '-'}
-        out = render(self.table, params)
-        self.assertTrue(out.equals(self.table))  # should NOP when first applied
+    def test_str(self):
+        result = render(
+            pd.DataFrame({'A': ['a', 'b'], 'B': ['c', 'd']}),
+            P('A', 'B', '-', 'C')
+        )
+        expected = pd.DataFrame({
+            'A': ['a', 'b'],
+            'B': ['c', 'd'],
+            'C': ['a-c', 'b-d'],
+        })
+        assert_frame_equal(result, expected)
 
-        params = { 'firstcolumn': 'catcol', 'secondcolumn': 'intcol', 'newcolumn': '', 'delimiter': '-'}
-        out = render(self.table, params)
-        self.assertTrue(out.equals(self.table))  # should NOP when first applied
+    def test_ignore_table_ordering(self):
+        result = render(
+            pd.DataFrame({'A': ['a', 'b'], 'B': ['c', 'd']}),
+            P('B', 'A', '-', 'C')
+        )
+        expected = pd.DataFrame({
+            'A': ['a', 'b'],
+            'B': ['c', 'd'],
+            'C': ['c-a', 'd-b'],
+        })
+        assert_frame_equal(result, expected)
 
-    def test_string(self):
-        params = {'firstcolumn': 'stringcol1', 'secondcolumn': 'stringcol2', 'newcolumn': 'newcol', 'delimiter': '-'}
-        out = render(self.table, params)
-        ref = self.table
-        ref['newcol'] = pd.Series(['a-b', 'a-b', 'a-', 'a-', 'a-b'])
-        self.assertTrue(out.equals(ref))
+    def test_overwrite_newcolumn(self):
+        result = render(
+            pd.DataFrame({'A': ['a', 'b'], 'B': ['c', 'd']}),
+            P('A', 'B', '-', 'A')
+        )
+        expected = pd.DataFrame({
+            'A': ['a-c', 'b-d'],
+            'B': ['c', 'd'],
+        })
+        assert_frame_equal(result, expected)
 
-    def test_cat(self):
-        params = {'firstcolumn': 'stringcol1', 'secondcolumn': 'catcol', 'newcolumn': 'newcol', 'delimiter': '-'}
-        out = render(self.table, params)
-        ref = self.table
-        ref['newcol'] = pd.Series(['a-', 'a-b', 'a-c', 'a-d', 'a-e'])
-        self.assertTrue(out.equals(ref))
+    def test_str_ignore_nan(self):
+        result = render(
+            # Test for silly tricks involving str(np.nan)....
+            pd.DataFrame({
+                'A': [np.nan, np.nan, '3', '4', 'nan'],
+                'B': ['1', np.nan, '3', 'nan', '5'],
+            }),
+            P('A', 'B', '-', 'C')
+        )
+        expected = pd.DataFrame({
+            'A': [np.nan, np.nan, '3', '4', 'nan'],
+            'B': ['1', np.nan, '3', 'nan', '5'],
+            'C': ['1', np.nan, '3-3', '4-nan', 'nan-5'],
+        })
+        assert_frame_equal(result, expected)
 
-    def test_num(self):
-        params = {'firstcolumn': 'intcol', 'secondcolumn': 'floatcol', 'newcolumn': 'newcol', 'delimiter': '-'}
-        out = render(self.table, params)
-        ref = self.table
-        ref['newcol'] = pd.Series(['1-2', '1-', '1-', '1-2', '1-2'])
-        self.assertTrue(out.equals(ref))
+    def test_categorical(self):
+        result = render(
+            pd.DataFrame({'A': ['a', 'b'], 'B': ['c', 'd']}, dtype='category'),
+            P('A', 'B', '-', 'A')
+        )
+        # Let's just convert to str, for now
+        expected = pd.DataFrame({
+            'A': ['a-c', 'b-d'],
+            'B': pd.Series(['c', 'd'], dtype='category'),
+        })
+        assert_frame_equal(result, expected)
 
-    def test_col_overwrite(self):
-        params = {'firstcolumn': 'stringcol1', 'secondcolumn': 'stringcol2', 'newcolumn': 'stringcol2', 'delimiter': '-'}
-        out = render(self.table, params)
-        ref = self.table
-        ref['stringcol2'] = pd.Series(['a-b', 'a-b', 'a-', 'a-', 'a-b'])
-        self.assertTrue(out.equals(ref))
+    def test_categorical_ignore_nan(self):
+        result = render(
+            # Test for silly tricks involving str(np.nan)....
+            pd.DataFrame({
+                'A': [np.nan, '2'],
+                'B': ['1', '3'],
+            }, dtype='category'),
+            P('A', 'B', '-', 'C')
+        )
+        expected = pd.DataFrame({
+            'A': pd.Series([np.nan, '2'], dtype='category'),
+            'B': pd.Series(['1', '3'], dtype='category'),
+            'C': ['1', '2-3'],
+        })
+        assert_frame_equal(result, expected)
+
 
 if __name__ == '__main__':
     unittest.main()
-
-
